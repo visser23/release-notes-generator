9be05af795640f2a5589b3fd0dc43f42604a2db8
Visser23 <viss23@hotmail.com>
2023-06-21 12:01:21 +0100

    Updated script - added webhook details

--- report-generator.py
+++ report-generator.py
@@ -1,52 +1,55 @@
 import requests
 from docx import Document
 
-# Jira and Confluence details
-JIRA_BASE_URL = 'https://servita-uk.atlassian.net/jira'
-JIRA_API_USERNAME = 'matt.visser@servita.com'
-JIRA_API_TOKEN = 'ATATT3xFfGF0h1WuFZJy9jXFViCDlYVYILzWtqukLDHbEH2_KJIeP2zMx5m4yF2ENPYnUkMfaRRw9pSgDx8bdS3DH_bqAGglcsIF8RetSA4CxR2DSxIvmJXap-xrarRh4lyigKPnF-fyV75L34pyuedQws8Nv9RLXqwePRCz3BjuA_eQ16aDYJ0=5A479038'
-CONFLUENCE_BASE_URL = 'https://servita-uk.atlassian.net/wiki'
-CONFLUENCE_PAGE_ID = '139853825'
-CONFLUENCE_API_USERNAME = 'matt.visser@servita.com'
-CONFLUENCE_API_TOKEN = 'ATATT3xFfGF0h1WuFZJy9jXFViCDlYVYILzWtqukLDHbEH2_KJIeP2zMx5m4yF2ENPYnUkMfaRRw9pSgDx8bdS3DH_bqAGglcsIF8RetSA4CxR2DSxIvmJXap-xrarRh4lyigKPnF-fyV75L34pyuedQws8Nv9RLXqwePRCz3BjuA_eQ16aDYJ0=5A479038'
+def generate_release_notes(request):
+    payload = request.json
+    released_fix_version = payload['fixVersion']['name']
+    logo_url = "https://nhshealthcall.co.uk/wp-content/themes/health_call_2022/images/logo_health_call.png"
 
-def generate_release_notes(released_fix_version):
-    # Retrieve Jira issues for the released fix version
-    issues_url = f'{JIRA_BASE_URL}/rest/api/2/search'
-    query = f'fixVersion="{released_fix_version}"'
-    headers = {'Content-Type': 'application/json'}
-    auth = (JIRA_API_USERNAME, JIRA_API_TOKEN)
-    payload = {'jql': query, 'fields': 'summary,description'}
-    response = requests.get(issues_url, headers=headers, auth=auth, params=payload)
-    issues = response.json().get('issues', [])
+    # Retrieve issue details from Jira
+    jira_issues_url = f"https://servita-uk.atlassian.net/jira/rest/api/2/search"
+    jira_headers = {
+        "Authorization": "Bearer ATATT3xFfGF0h1WuFZJy9jXFViCDlYVYILzWtqukLDHbEH2_KJIeP2zMx5m4yF2ENPYnUkMfaRRw9pSgDx8bdS3DH_bqAGglcsIF8RetSA4CxR2DSxIvmJXap-xrarRh4lyigKPnF-fyV75L34pyuedQws8Nv9RLXqwePRCz3BjuA_eQ16aDYJ0=5A479038",
+        "Content-Type": "application/json"
+    }
+    jira_payload = {
+        "jql": f"fixVersion = '{released_fix_version}'",
+        "fields": "summary"
+    }
+    jira_response = requests.get(jira_issues_url, headers=jira_headers, json=jira_payload)
+    issues = jira_response.json().get("issues", [])
 
     # Generate the release notes document
     doc = Document()
-    doc.add_heading('Release Notes', level=1)
+    doc.add_picture(logo_url)
+    doc.add_heading(f"Release Notes - {released_fix_version}", level=1)
     for issue in issues:
-        issue_key = issue['key']
-        summary = issue['fields']['summary']
-        description = issue['fields']['description']
-        doc.add_heading(issue_key, level=2)
-        doc.add_paragraph(f'Summary: {summary}')
-        doc.add_paragraph(f'Description: {description}')
-        doc.add_paragraph('')  # Add an empty line between issues
+        summary = issue["fields"]["summary"]
+        doc.add_heading(summary, level=2)
 
-    # Save the release notes document
-    doc.save('release_notes.docx')
+    # Save and upload the release notes document to Confluence
+    doc.save("/tmp/release_notes.docx")
+    confluence_url = "https://servita-uk.atlassian.net/wiki/rest/api/content/139853825/child/attachment"
+    confluence_headers = {
+        "Authorization": "Bearer ATATT3xFfGF0h1WuFZJy9jXFViCDlYVYILzWtqukLDHbEH2_KJIeP2zMx5m4yF2ENPYnUkMfaRRw9pSgDx8bdS3DH_bqAGglcsIF8RetSA4CxR2DSxIvmJXap-xrarRh4lyigKPnF-fyV75L34pyuedQws8Nv9RLXqwePRCz3BjuA_eQ16aDYJ0=5A479038",
+        "X-Atlassian-Token": "no-check"
+    }
+    confluence_params = {
+        "minorEdit": "true"
+    }
+    confluence_files = {
+        "file": open("/tmp/release_notes.docx", "rb")
+    }
+    confluence_response = requests.post(confluence_url, headers=confluence_headers, params=confluence_params, files=confluence_files)
 
-    # Update the Confluence page with the release notes document
-    confluence_url = f'{CONFLUENCE_BASE_URL}/wiki/rest/api/content/{CONFLUENCE_PAGE_ID}/child/attachment'
-    headers = {'X-Atlassian-Token': 'no-check'}
-    auth = (CONFLUENCE_API_USERNAME, CONFLUENCE_API_TOKEN)
-    files = {'file': open('release_notes.docx', 'rb')}
-    response = requests.post(confluence_url, headers=headers, auth=auth, files=files)
-
-    if response.status_code == 200:
-        print('Release notes generated and uploaded successfully.')
+    if confluence_response.status_code == 200:
+        return {"message": "Release notes generated and uploaded successfully."}
     else:
-        print('Failed to upload release notes.')
+        return {"message": "Failed to upload release notes."}
 
-# Example usage
-released_fix_version = '1.0.0'  # Replace with the actual released fix version
-generate_release_notes(released_fix_version)
+# Vercel API route
+def webhook(request):
+    if request.method == "POST":
+        return generate_release_notes(request)
+    else:
+        return {"message": "Invalid request method."}
